e358baa37f0a20bf114774079c193945a9660f00
emretasss <tas7095@gmail.com>
2024-12-05 08:11:41 +0300

    "Removed Cta, FinancialBlog, and Testimonial components from Page.jsx; refactored EasyStepFeature component to simplify layout and remove unnecessary elements."

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,18 @@
 import Footer from '@/components/footer/Footer'
-import Cta from '@/components/home-1/Cta'
 import Faq from '@/components/home-1/Faq'
 import Features from '@/components/home-1/Features'
 import Hero from '@/components/home-1/Hero'
 import Integration from '@/components/home-1/Integration'
-
-
-
-
 import PrimaryNavbar from '@/components/navbar/PrimaryNavbar'
 import Clients from '@/components/shared/Clients'
-
-
-
-import FinancialBlog from '@/components/shared/FinancialBlog'
-
-
-import Testimonial from '@/components/shared/Testimonial'
 import EasyStepFeature from '@/components/home-1/EasyStepFeature'
-
 export const metadata = {
   title: 'Castrum Legions',
 }
 
 export default function Home() {
   return (
-    <>
+    <>  
       <PrimaryNavbar />
       <main>
         <Hero />
@@ -41,9 +28,9 @@ export default function Home() {
        
 
         <Faq />
-        <Testimonial />
-        <FinancialBlog />
-        <Cta />
+       
+      
+       
       </main>
       <Footer />
     </>
--- components/home-1/EasyStepFeature.jsx
+++ components/home-1/EasyStepFeature.jsx
@@ -27,10 +27,7 @@ const easyStepPlan1 = [
     description:
       ' We offer a variety of secure payment methods to make your payment process convenient and hassle-free.',
   },
-  
-  
 ]
-
 
 const easyStepPlan2 = [
   {
@@ -51,133 +48,118 @@ const easyStepPlan2 = [
     description:
       ' We offer a variety of secure payment methods to make your payment process convenient and hassle-free.',
   },
-  
-  
 ]
 
 const EasyStepFeature = () => {
   const ref = useRef(null)
   const controlAnimation = useWhileInView(ref)
 
-  return (<>
-  
-  
-  
-    <section className="relative overflow-hidden bg-black">
-      <div className="container relative z-10 pt-16 pb-24">
-        <div className="mx-auto mb-16 max-w-[550px] text-center text-white">
-          <p className="section-tagline"></p>
-          <p className="section-tagline"></p>
-          <h2 className="text-white">
-            What&apos;s the procedure for <br />
-            its operation?
-          </h2>
-        </div>
+  return (
+    <>
+      <section className="relative overflow-hidden bg-black">
+        <div className="container relative z-10 pt-16 pb-24">
+          <div className="mx-auto mb-16 max-w-[550px] text-center text-white">
+            <h2 className="text-white">
+              What&apos;s the procedure for <br />
+              its operation?
+            </h2>
+          </div>
 
-        <div className="grid grid-cols-2 items-center gap-20 max-md:grid-cols-1">
-          <div className="[&amp;>*:not(:last-child)]:mb-10 max-md:order-1">
-            {easyStepPlan1?.map((plan, i) => (
-              <FadeUpOneByOneAnimation
-                i={i}
-                className="rounded-medium bg-gray-800 p-4 shadow-box dark:bg-dark-200"
-                key={plan.id}>
-                <div className="rounded border border-dashed border-gray-600 p-8 max-md:p-4">
-                  <div className="flex gap-10 max-lg:gap-5">
-                    <div className="after:border-borderColour-dark relative flex size-10 shrink-0 items-center justify-center rounded-full bg-primary after:absolute after:left-1/2 after:top-1/2 after:size-[30px] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:border after:border-dashed">
-                      <p className="font-jakarta_sans font-bold text-paragraph dark:text-paragraph">{plan.id}</p>
-                    </div>
-                    <div>
-                      <h3 className="mb-2.5 text-white">{plan.title}</h3>
-                      <p className="text-gray-300">{plan.description}</p>
+          <div className="grid grid-cols-2 items-center gap-20 max-md:grid-cols-1">
+            <div className="[&amp;>*:not(:last-child)]:mb-10 max-md:order-1">
+              {easyStepPlan1?.map((plan, i) => (
+                <FadeUpOneByOneAnimation
+                  i={i}
+                  className="rounded-medium bg-gray-800 p-4 shadow-box dark:bg-dark-200"
+                  key={plan.id}>
+                  <div className="rounded p-8 max-md:p-4">
+                    <div className="flex gap-10 max-lg:gap-5">
+                      <div className="relative flex size-10 shrink-0 items-center justify-center rounded-full bg-primary">
+                        <p className="font-jakarta_sans font-bold text-paragraph dark:text-paragraph">{plan.id}</p>
+                      </div>
+                      <div>
+                        <h3 className="mb-2.5 text-white">{plan.title}</h3>
+                        <p className="text-gray-300">{plan.description}</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </FadeUpOneByOneAnimation>
-            ))}
+                </FadeUpOneByOneAnimation>
+              ))}
+            </div>
+            <motion.div
+              className="relative flex aspect-square items-center justify-end max-md:justify-center"
+              initial="initial"
+              animate={controlAnimation}
+              variants={fadeUpAnimation}
+              ref={ref}>
+              <Image
+                src={hostingStep}
+                alt="company image"
+                className="aos-init aos-animate dark:hidden"
+                placeholder="blur"
+                quality={70}
+              />
+              <Image
+                src={hostingStepDark}
+                alt="company image"
+                className="aos-init aos-animate hidden dark:inline-block"
+                placeholder="blur"
+                quality={70}
+              />
+            </motion.div>
           </div>
-          <motion.div
-            className="relative flex aspect-square items-center justify-end max-md:justify-center"
-            initial="initial"
-            animate={controlAnimation}
-            variants={fadeUpAnimation}
-            ref={ref}>
-            <Image
-              src={hostingStep}
-              alt="company image"
-              className="aos-init aos-animate dark:hidden"
-              placeholder="blur"
-              quality={70}
-            />
-            <Image
-              src={hostingStepDark}
-              alt="company image"
-              className="aos-init aos-animate hidden dark:inline-block"
-              placeholder="blur"
-              quality={70}
-            />
-            <div className="absolute left-1/2 top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 after:absolute after:left-1/2 after:top-1/2 after:h-[465px] after:w-[465px] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:bg-primary-200/30 after:blur-[60px]"></div>
-          </motion.div>
         </div>
-      </div>
-    </section>
+      </section>
 
+      <section className="relative overflow-hidden bg-black pt-16 pb-24">
+        <div className="container relative z-10">
+          <div className="grid grid-cols-2 items-center gap-20 max-md:grid-cols-1">
+            <motion.div
+              className="relative flex aspect-square items-center justify-start max-md:justify-center"
+              initial="initial"
+              animate={controlAnimation}
+              variants={fadeUpAnimation}
+              ref={ref}>
+              <Image
+                src={hostingStep}
+                alt="company image"
+                className="aos-init aos-animate dark:hidden"
+                placeholder="blur"
+                quality={70}
+              />
+              <Image
+                src={hostingStepDark}
+                alt="company image"
+                className="aos-init aos-animate hidden dark:inline-block"
+                placeholder="blur"
+                quality={70}
+              />
+            </motion.div>
 
-    <section className="relative overflow-hidden bg-black pt-16 pb-24">
-      <div className="container relative z-10">
-      
-
-        <div className="grid grid-cols-2 items-center gap-20 max-md:grid-cols-1">
-          {/* Resim Alanı */}
-          <motion.div
-            className="relative flex aspect-square items-center justify-start max-md:justify-center"
-            initial="initial"
-            animate={controlAnimation}
-            variants={fadeUpAnimation}
-            ref={ref}>
-            <Image
-              src={hostingStep}
-              alt="company image"
-              className="aos-init aos-animate dark:hidden"
-              placeholder="blur"
-              quality={70}
-            />
-            <Image
-              src={hostingStepDark}
-              alt="company image"
-              className="aos-init aos-animate hidden dark:inline-block"
-              placeholder="blur"
-              quality={70}
-            />
-            <div className="absolute left-1/2 top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 after:absolute after:left-1/2 after:top-1/2 after:h-[465px] after:w-[465px] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:bg-primary-200/30 after:blur-[60px]"></div>
-          </motion.div>
-
-          {/* Yazı Alanı */}
-          <div className="[&amp;>*:not(:last-child)]:mb-10 max-md:order-2">
-            {easyStepPlan2?.map((plan, i) => (
-              <FadeUpOneByOneAnimation
-                i={i}
-                className="rounded-medium bg-gray-800 p-4 shadow-box dark:bg-dark-200"
-                key={plan.id}>
-                <div className="rounded border border-dashed border-gray-600 p-8 max-md:p-4">
-                  <div className="flex gap-10 max-lg:gap-5">
-                    <div className="after:border-borderColour-dark relative flex size-10 shrink-0 items-center justify-center rounded-full bg-primary after:absolute after:left-1/2 after:top-1/2 after:size-[30px] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:border after:border-dashed">
-                      <p className="font-jakarta_sans font-bold text-paragraph dark:text-paragraph">{plan.id}</p>
-                    </div>
-                    <div>
-                      <h3 className="mb-2.5 text-white">{plan.title}</h3>
-                      <p className="text-gray-300">{plan.description}</p>
+            <div className="[&amp;>*:not(:last-child)]:mb-10 max-md:order-2">
+              {easyStepPlan2?.map((plan, i) => (
+                <FadeUpOneByOneAnimation
+                  i={i}
+                  className="rounded-medium bg-gray-800 p-4 shadow-box dark:bg-dark-200"
+                  key={plan.id}>
+                  <div className="rounded p-8 max-md:p-4">
+                    <div className="flex gap-10 max-lg:gap-5">
+                      <div className="relative flex size-10 shrink-0 items-center justify-center rounded-full bg-primary">
+                        <p className="font-jakarta_sans font-bold text-paragraph dark:text-paragraph">{plan.id}</p>
+                      </div>
+                      <div>
+                        <h3 className="mb-2.5 text-white">{plan.title}</h3>
+                        <p className="text-gray-300">{plan.description}</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </FadeUpOneByOneAnimation>
-            ))}
+                </FadeUpOneByOneAnimation>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
- 
- 
-</>
+      </section>
+    </>
   )
 }
 
