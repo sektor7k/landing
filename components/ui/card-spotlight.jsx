4cce033432c567d3bcd11f821ad894c4f23d858c
emretasss <tas7095@gmail.com>
2024-12-22 05:33:32 +0300

    `fix typo in import path: CanvasRevealEffect`

--- components/ui/card-spotlight.jsx
+++ components/ui/card-spotlight.jsx
@@ -2,7 +2,7 @@
 
 import { useMotionValue, motion, useMotionTemplate } from 'framer-motion'
 import React, { useState } from 'react'
-import { CanvasRevealEffect } from '@/components/ui/Canvas-reveal-effect'
+import { CanvasRevealEffect } from '@/components/ui/canvas-reveal-effect'
 import { cn } from '@/lib/utils'
 
 export const CardSpotlight = ({ children, radius = 350, color = '#262626', className, ...props }) => {
