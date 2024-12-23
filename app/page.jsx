56884d22b8047fc5ad5281460b42ccd7a8a4852d
emretasss <tas7095@gmail.com>
2024-12-23 07:52:51 +0300

    sd

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,4 @@
 import Footer from '@/components/footer/Footer'
-import Faq from '@/components/home-1/Faq'
 import Features from '@/components/home-1/Features'
 import Hero from '@/components/home-1/Hero'
 import SimpleNavbarWithHoverEffects from '@/components/navbar/SimpleNavbarWithHoverEffects'
@@ -10,7 +9,7 @@ import { Roadmap } from '@/components/home-1/Roadmap'
 import { Advisor } from '@/components/Advisor/Advisor'
 import { Ecosystem } from '@/components/Ecosystem/Ecosystem'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import { FrequentlyAskedQuestionsAccordion } from "@/components/ui/FrequentlyAskedQuestionsAccordion"
+import { FrequentlyAskedQuestionsAccordion } from '@/components/ui/FrequentlyAskedQuestionsAccordion'
 
 export const metadata = {
   title: 'Castrum Legions',
